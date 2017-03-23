f4b156d32e26f381109dfdcb7d879d20b85520da
timdpr <qotimsa@live.co.uk>
2017-03-23 21:51:02 +0000

    Add Smileys

---
+++ week7-week7_01.Smileys/src/Smileys.java
@@ -0,0 +1,30 @@
+public class Smileys {
+
+    public static void main(String[] args) {
+        printWithSmileys("Method");
+        printWithSmileys("Beerbottle");
+        printWithSmileys("Interface");
+    }
+    
+    private static void printWithSmileys(String characterString) {
+        String middleRow = makeMiddleRow(characterString);
+
+        printSmileyLine(middleRow.length() / 2);
+        System.out.println(middleRow);
+        printSmileyLine(middleRow.length() / 2);
+    }
+    
+    private static void printSmileyLine(int length) {
+        for (int i = 0; i < length; i++) {
+            System.out.print(":)");
+        }
+        System.out.println("");
+    }
+    
+    private static String makeMiddleRow(String characterString) {
+        if (characterString.length() % 2 == 1) {
+            return ":) " + characterString + "  :)";
+        }
+        return ":) " + characterString + " :)";
+    }
+}
