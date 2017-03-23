306d408df0771a21c8721f8d74d55b5327afd04f
timdpr <qotimsa@live.co.uk>
2017-03-23 21:51:17 +0000

    Add CharacterStringChanger

---
+++ week7-week7_02.CharacterStringChanger/src/Change.java
@@ -0,0 +1,25 @@
+public class Change {
+    private final char fromCharacter;
+    private final char toCharacter;
+    
+    public Change(char fromCharacter, char toCharacter) {
+        this.fromCharacter = fromCharacter;
+        this.toCharacter = toCharacter;
+    }
+    
+    public String change(String characterString) {
+        /*
+        String returnString = "";
+        for (int i = 0; i < characterString.length(); i++) {
+            char c = characterString.charAt(i);
+            if (characterString.charAt(i) == fromCharacter) {
+                returnString = returnString + toCharacter;
+            } else {
+                returnString = returnString + c;
+            }
+        }
+        return returnString;
+        */
+        return characterString.replace(fromCharacter, toCharacter);
+    }
+}
---
+++ week7-week7_02.CharacterStringChanger/src/Changer.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Changer {
+    private final ArrayList<Change> changeList = new ArrayList<Change>();
+    
+    public void addChange(Change change) {
+        changeList.add(change);
+    }
+    
+    public String change(String characterString) {
+        for (Change ch : changeList) {
+            characterString = ch.change(characterString);
+        }
+        return characterString;
+    }
+}
