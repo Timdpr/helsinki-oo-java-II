014025d3e6bea8b39169d08bf6841851ab51d2b2
timdpr <qotimsa@live.co.uk>
2017-04-16 18:25:54 +0100

    Add TheFinnishRingingCentre

---
+++ week10-week10_27.TheFinnishRingingCentre/src/Bird.java
@@ -0,0 +1,39 @@
+public class Bird {
+
+    private String name;
+    private String latinName;
+    private int ringingYear;
+
+    public Bird(String name, String latinName, int ringingYear) {
+        this.name = name;
+        this.latinName = latinName;
+        this.ringingYear = ringingYear;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.latinName + " (" + this.ringingYear + ")";
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        if(this.getClass() != other.getClass()){
+            return false;
+        }
+        Bird compared = (Bird) other;
+        return compared.latinName.equals(this.latinName) &&
+               compared.ringingYear == this.ringingYear;
+    }
+    
+    @Override
+    public int hashCode() {
+        if (this.latinName == null) {
+            return 7;
+        }
+        return this.ringingYear + this.latinName.hashCode();
+    }
+}
---
+++ week10-week10_27.TheFinnishRingingCentre/src/RingingCentre.java
@@ -0,0 +1,30 @@
+/*
+ * @author Timdpr
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RingingCentre {
+    private Map<Bird, ArrayList<String>> placeMap = new HashMap<Bird, ArrayList<String>>();
+    
+    public void observe(Bird bird, String place) {
+        if (!this.placeMap.containsKey(bird)) {
+            this.placeMap.put(bird, new ArrayList<String>());
+        }
+        this.placeMap.get(bird).add(place);
+    }
+    
+    public void observations(Bird bird) {
+        if (this.placeMap.get(bird) == null) {
+            System.out.println(bird + " observations: 0");
+        } else {
+            int obsNum = this.placeMap.get(bird).size();
+            System.out.println(bird + " observations: " + obsNum);
+            for (int i = 0; i < obsNum; i++) {
+                System.out.println(placeMap.get(bird).get(i));
+            }
+        }
+    }
+}
